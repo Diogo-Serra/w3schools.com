70836dd50feb3446574027e47739d13e99a533f0
Diogo Alexandre Soares Serra <di-code@outlook.com>
2026-02-27 17:04:27 +0000

    updates

--- 1_collections/lists.py
+++ 1_collections/lists.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
 
+"""
+List is a collection which is ordered and changeable. Allows duplicate members.
+Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
+Set is a collection which is unordered, unchangeable*, and unindexed. No duplicate members.
+Dictionary is a collection which is ordered** and changeable. No duplicate members.
+"""
+
 print("All set")
 
 thislist = ["Apple", "banana", "Cherry"]
@@ -17,3 +24,5 @@ print(type(list1))
 
 thislist = list(("apple", "Apple"))
 print(thislist)
+
+print(thislist[0])
