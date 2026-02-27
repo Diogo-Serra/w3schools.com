d1daec0b76f265f3399d4670b0908132a57be74c
Diogo Alexandre Soares Serra <di-code@outlook.com>
2026-02-27 11:54:38 +0000

    updates

--- 0_basics/hello_world.py
+++ 0_basics/hello_world.py
@@ -3,9 +3,13 @@ import sys
 
 print(sys.version)
 
+# This is comment!
+
 # Print "Hello World!"
 print("Hello World!")
 # Print "Have a good day."
 print("Have a good day.")
 # Print "Learning Python is fun!"
 print("Learning Python is fun!")
+
+print("Print1") ; print("Print2")
