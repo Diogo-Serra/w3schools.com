35d29a0be3568ff250c950527c182c32a28d68ba
Diogo-Serra <di-code@outlook.com>
2026-02-27 00:11:21 +0000

    updates

--- 0_basics/hello_world.py
+++ 0_basics/hello_world.py
@@ -3,3 +3,9 @@ import sys
 
 print(sys.version)
 
+# Print "Hello World!"
+print("Hello World")
+# Print "Have a good day."
+print("Have a good day.")
+# Print "Learning Python is fun!"
+print("Learning Python is fun!")
