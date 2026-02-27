64707217900e8a3abdd426146bf21dfca9a080d2
Diogo Alexandre Soares Serra <di-code@outlook.com>
2026-02-27 16:44:07 +0000

    updates

--- 0_basics/random_number.py
+++ 0_basics/random_number.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python3
 
+"""
+== 	Equal 	x == y 	
+!= 	Not equal 	x != y 	
+> 	Greater than 	x > y 	
+< 	Less than 	x < y 	
+>= 	Greater than or equal to 	x >= y 	
+<= 	Less than or equal to 	x <= y
+"""
+
 import random
 
 print(random.randrange(1, 10))
