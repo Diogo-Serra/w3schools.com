9890254dd0e9e3f062134200c3c20ce4a2568671
Diogo Alexandre Soares Serra <di-code@outlook.com>
2026-02-27 14:36:08 +0000

    updates

--- 0_basics/variables.py
+++ 0_basics/variables.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 
 # Variables should have descriptive names
+"""
+A variable name must start with a letter or the underscore character
+A variable name cannot start with a number
+A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+Variable names are case-sensitive (age, Age and AGE are three different variables)
+A variable name cannot be any of the Python keywords.
+"""
 
 x = 4
 y = "Sally"
