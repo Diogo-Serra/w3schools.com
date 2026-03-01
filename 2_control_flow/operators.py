025034cba5f3ddd1b9f9a9c4320b874078c672f2
Diogo Alexandre Soares Serra <di-code@outlook.com>
2026-03-01 17:03:52 +0000

    updates

--- 2_control_flow/operators.py
+++ 2_control_flow/operators.py
@@ -44,3 +44,11 @@ match day:
         print("found")
     case _:
         print("other")
+
+
+day = 4
+match day:
+    case 1 | 2 | 3 | 4 | 5:
+        print("Today is a weekday")
+    case 6 | 7:
+        print("I love weekends!")
