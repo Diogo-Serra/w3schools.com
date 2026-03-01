6ea8d6610dce09661e681d7f433a84e426009eb6
Diogo Alexandre Soares Serra <di-code@outlook.com>
2026-03-01 18:17:34 +0000

    updates

--- 3_functions_modules/functions.py
+++ 3_functions_modules/functions.py
@@ -7,3 +7,12 @@ def my_function():
 my_function()
 my_function()
 my_function()
+
+
+def fahrenheit_to_celsius(fahrenheit):
+    return (fahrenheit - 32) * 5 / 9
+
+
+print(fahrenheit_to_celsius(77))
+print(fahrenheit_to_celsius(50))
+print(type(fahrenheit_to_celsius(50)))
