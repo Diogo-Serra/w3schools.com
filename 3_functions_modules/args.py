0344935ab72af699a2de0d1d20fd019bed36f956
Diogo Alexandre Soares Serra <di-code@outlook.com>
2026-03-01 18:53:29 +0000

    updates

--- 3_functions_modules/args.py
+++ 3_functions_modules/args.py
@@ -10,4 +10,18 @@ def my_function(*numbers):
         print(number)
 
 
-my_function(52, 2, 3, 12, 4)
+# my_function(52, 2, 3, 12, 4)
+
+
+def max_function(*numbers):
+    if (len(numbers) == 0):
+        return None
+    max_num = numbers[0]
+    for num in numbers:
+        if num > max_num:
+            max_num = num
+    return max_num
+
+
+max = max_function(100, 4, 55, 21, 212)
+print(max)
